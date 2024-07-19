402f9b286a8ab924a3432403a2aba6d7311a61e7
Nitesh-Baniya <niteshofficial876@gmail.com>
2024-07-19 16:28:23 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "Nitesh-Baniya",
+    githubLink: "https://github.com/Nitesh-Baniya" ,
+    email : "",
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
